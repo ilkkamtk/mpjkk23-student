84ba569ec1f8ccd8372cdd8ddb166138559869f1
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-16 11:27:29 +0200

    first-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import MediaTable from './components/MediaTable';
 
-function App() {
-  const [count, setCount] = useState(0)
+const mediaArray = [
+  {
+    title: 'Title 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/161',
+    },
+    filename: 'http://placekitten.com/2048/1920',
+  },
+  {
+    title: 'Title 2',
+    description:
+      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/162',
+    },
+    filename: 'http://placekitten.com/2041/1922',
+  },
+  {
+    title: 'Title 3',
+    description:
+      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/163',
+    },
+    filename: 'http://placekitten.com/2039/1920',
+  },
+];
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const App = () => {
+  return <MediaTable mediaArray={mediaArray} />;
+};
 
-export default App
+export default App;
---
+++ src/components/MediaRow.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+const MediaRow = ({file}) => {
+  return (
+    <tr>
+      <td>
+        <img src={file.thumbnails.w160} alt={file.title} />
+      </td>
+      <td>
+        <h3>{file.title}</h3>
+        <p>{file.description}</p>
+      </td>
+      <td>
+        <a href={file.filename}>View</a>
+      </td>
+    </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+export default MediaRow;
