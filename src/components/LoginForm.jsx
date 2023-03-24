b96cf061a68ff4f8d4dd7101056cbcf42a51c62c
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-24 09:27:10 +0200

    semiredi

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,41 @@
 import PropTypes from 'prop-types';
+import useForm from '../hooks/FormHooks';
 
 const LoginForm = (props) => {
-  return <div>LoginForm</div>;
+  const initValues = {
+    username: '',
+    password: '',
+  };
+
+  const doLogin = () => {
+    console.log('submitted', inputs);
+  };
+
+  const {inputs, handleSubmit, handleInputChange} = useForm(
+    doLogin,
+    initValues
+  );
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="username"
+          placeholder="Username"
+          onChange={handleInputChange}
+          value={inputs.username}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleInputChange}
+          value={inputs.password}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 LoginForm.propTypes = {};
