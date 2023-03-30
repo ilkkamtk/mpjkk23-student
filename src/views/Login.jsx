fbb23bdb0e67241bda68d6b559fbe2b7c7321b65
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-30 11:36:19 +0300

    mui

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,4 @@
-import {Grid} from '@mui/material';
+import {Button, Grid} from '@mui/material';
 import PropTypes from 'prop-types';
 import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
@@ -18,7 +18,7 @@ const Login = (props) => {
         <p>{formToggle ? 'First time here?' : 'or'}</p>
       </Grid>
       <Grid item xs={6}>
-        <button onClick={toggle}>{formToggle ? 'Register' : 'Login'}</button>
+        <Button onClick={toggle}>{formToggle ? 'Register' : 'Login'}</Button>
       </Grid>
     </Grid>
   );
