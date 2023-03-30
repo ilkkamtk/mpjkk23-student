3d7645e02ba04bc19fc5263bfbbb5a584b9eae2a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-30 12:50:36 +0300

    mui

--- src/theme/themeOptions.js
+++ src/theme/themeOptions.js
@@ -3,16 +3,16 @@ import '@fontsource/lato';
 
 const themeOptions = {
   typography: {
-    fontFamily: 'Playfair Display',
-  },
-  body1: {
     fontFamily: 'Lato',
-  },
-  body2: {
-    fontFamily: 'Lato',
-  },
-  button: {
-    fontFamily: 'Lato',
+    h1: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h2: {
+      fontFamily: 'Playfair Display, serif',
+    },
+    h3: {
+      fontFamily: 'Playfair Display, serif',
+    },
   },
 };
 
