6cdbeef759109d3379626da7d3982ed38bf18428
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-04-06 09:43:38 +0300

    upload start

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,13 +1,43 @@
 import {Box, Button} from '@mui/material';
 import PropTypes from 'prop-types';
+import useForm from '../hooks/FormHooks';
 
 const Upload = (props) => {
+  const initValues = {
+    title: '',
+    description: '',
+    file: '',
+  };
+
+  const doUpload = () => {};
+
+  const {inputs, handleSubmit, handleInputChange} = useForm(
+    doUpload,
+    initValues
+  );
+
+  //console.log('Upload', inputs);
+
   return (
     <Box>
       <form>
-        <input type="text" name="title" value="title"></input>
-        <textarea name="textarea"></textarea>
-        <input type="file" name="file" accept="image/* video/* audio/*"></input>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          name="title"
+          value={inputs.title}
+        ></input>
+        <textarea
+          onChange={handleInputChange}
+          name="description"
+          value={inputs.description}
+        ></textarea>
+        <input
+          onChange={handleInputChange}
+          type="file"
+          name="file"
+          accept="image/* video/* audio/*"
+        ></input>
         <Button type="submit">Upload</Button>
       </form>
     </Box>
